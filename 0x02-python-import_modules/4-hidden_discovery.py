08198765c542b64bb24f46bd4480f0145e02c62c
Irene-Busah <i.busah@alustudent.com>
2021-09-09 18:25:27 +0200

    new files

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for i in dir(hidden_4):
-        if i[0] != '_':
-            print("{}".format(i))
+    not_so_hidden = dir(hidden_4)
+    for s in not_so_hidden:
+        if s[0] != '_' and s[1] != '_':
+            print(s)
