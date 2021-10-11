36aec8a23af5cf812e3b5aebb66f5b95618ae210
Irene-Busah <i.busah@alustudent.com>
2021-10-11 22:21:14 +0200

    new files

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Defining a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Instantiating the methods of the class"""
+    def area(self):
+        raise Exception("area() is not implemented")
