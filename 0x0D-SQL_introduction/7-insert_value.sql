d746c22f56535fa51cb5c99d7538735e6e6486e7
Irene-Busah <i.busah@alustudent.com>
2021-11-17 00:04:11 +0200

    new files

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- The script inserts a new row in the table in the database
-INSERT INTO 'first_name' ('id', 'name') VALUES (89, 'Best School');
+INSERT INTO first_table (id, name) VALUES ('89', 'Best School');
