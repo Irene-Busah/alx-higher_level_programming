625cf67a5c0b0b1501e760d92bc92fbdd4858c56
Irene-Busah <i.busah@alustudent.com>
2021-11-17 23:50:01 +0200

    new files

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
---The script creates a database and a table on MySQL server
+-- Creates the database `hbtn_0d_usa` and
+-- the table `states` in MySQL Server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
-       id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-       name VARCHAR(256) NOT NULL
-       );
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL
+);
--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,11 @@
---The script creates a database and a table called cities on MySQL
+-- Creates the database `hbtn_0d_usa` and
+-- the table `cities` in MySQL Server
+-- with a FOREIGN KEY.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
-	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-	state_id INT NOT NULL, 
-	FOREIGN KEY (state_id) REFERENCES states(id), 
-	name VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS cities (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
