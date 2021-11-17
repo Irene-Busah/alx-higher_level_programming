25f95e28a8a8a493208172503099fdf88691a450
Irene-Busah <i.busah@alustudent.com>
2021-11-17 22:54:31 +0200

    new files

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- The script creates MySQL server user user_0d_1
-RANT ALL on *.* TO user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL on *.* TO user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
