8c526117efb7d4dda14ef25c7b7773e9a08f4433
Irene-Busah <i.busah@alustudent.com>
2021-11-17 23:37:58 +0200

    new files

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
---The script lists all shows contained in hbtn_0d_tvshows
-SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
-FROM tv_shows
-JOIN tv_show_genres
-WHERE tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;
+-- Lists all shows contained in `hbtn_0d_tvshows`
+-- that have at least one genre linked.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
