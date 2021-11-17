2a59668fafa76af380e4392dbf5902cb977af3d9
Irene-Busah <i.busah@alustudent.com>
2021-11-17 22:57:31 +0200

    new files

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,5 @@
 -- The script creates MySQL server user user_0d_1
-GRANT ALL on *.* TO user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+DROP USER IF EXISTS user_0d_1@localhost;
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+FLUSH PRIVILEGES;
