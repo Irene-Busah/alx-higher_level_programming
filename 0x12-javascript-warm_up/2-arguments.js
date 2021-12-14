ae79f53875316714684656d10d43e4b273a8009d
Irene-Busah <i.busah@alustudent.com>
2021-12-14 05:53:08 +0200

    new files

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-// This script prints a message depending on the number of arguments passed
-if (process.argv[3]){
-    console.log('Arguemnts found');
-} else if (process.argv[2]){
-    console.log('Argument found');
+
+if (process.argv.length > 3) {
+  console.log('Arguments found');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-    console.log('No argument');
+  console.log('No argument');
 }
