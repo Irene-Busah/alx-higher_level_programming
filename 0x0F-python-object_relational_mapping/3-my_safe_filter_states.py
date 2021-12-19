2a5f743d79d4bd23d70374b0ac426e28c15f0f21
Irene-Busah <i.busah@alustudent.com>
2021-12-19 23:26:04 +0200

    new files

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,41 +1,22 @@
 #!/usr/bin/python3
 """
-This script takes in an argument and
-displays all values in the states
-where `name` matches the argument
-from the database `hbtn_0e_0_usa`.
-This time the script is safe from
-MySQL injections!
+Script to list all states from database hbtn_0e_0_usa.
+Arguments are as follows: mysql username, mysql password, database name
 """
-
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
-    """
-
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
-
-    with db.cursor() as cur:
-        cur.execute("""
-            SELECT
-                *
-            FROM
-                states
-            WHERE
-                name LIKE BINARY %(name)s
-            ORDER BY
-                states.id ASC
-        """, {
-            'name': argv[4]
-        })
-
-        rows = cur.fetchall()
-
-    if rows is not None:
-        for row in rows:
-            print(row)
+if __name__ == "__main__":
+    """Getting data from hbtn_0e_0_usa"""
+    database = MySQLdb.connect(host='localhost', port=3306,
+                               user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = database.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY %s \
+    ORDER BY id ASC", (argv[4],))
+    list_of_states = cursor.fetchall()
+    """Print results"""
+    for states in list_of_states:
+        print(states)
+    """Close cursor and connection to database"""
+    cursor.close()
+    database.close()
