15a15a6b56ff4c01b54d7ec1280a2de9741940f6
Irene-Busah <i.busah@alustudent.com>
2022-01-11 03:39:54 +0200

    new files

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
-""" Github commits challenge """
-
+"""
+Python script that takes 2 arguments in order to solve this challenge
+"""
 import requests
 from sys import argv
 
-if __name__ == "__main__":
-    repo_name = argv[1]
-    owner_name = argv[2]
-    url = "https://api.github.com/repos/"+repo+"/"+owner+"/commits"
-    print("URL:", url)
-
-    try:
-        r = requests.get(url).json()
-        i = 0
-
-        while (i < 10):
-            print(r[i]['sha']+": "+r[i]['commit']['author']['name'])
-            i += 1
-    except:
-        pass
+if __name__ == '__main__':
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    r = requests.get(url)
+    commits = r.json()
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
